b750302aa307107ac8e5edf81395f88a2253af11
bambibextor <angelanliker@hotmail.com>
2015-06-12 09:03:20 +0200

    20

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position){
+		//hier suchen wir die aktuellen koordinaten
 
-		console.log(position);
-		$('.longitude').text(position.coords.longitude);
-		$('.latitude').text(position.coords.latitude);
-		$('.accuracy').text(position.coords.accuracy);
+			//ein Objekt
+			var koordinaten = { 
 
-	
-		});
+				longitude: position.coords.longitude,
+				latitude: position.coords.latitude
+
+			};
+		//in den Link meinen API Key einfügen, dieser sieht man, wenn man bei https://developer.forecast.io/ eingeloggt ist
+
+			$.ajax({
+				url: 'https://api.forecast.io/forecast/074dda45e7a36c5645570c91ba46be0f/'+ koordinaten.latitude +','+ koordinaten.longitude
+				data: {
+					units: 'si',
+					lang: 'de'
+				},
+			dataType: 'jsonp'
+
+			}).done(function(data){
+				console.log(data);
+
+			});
+	});
 });
+
