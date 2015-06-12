f94edfd962164ca543be026de70499bdb7c18910
bambibextor <angelanliker@hotmail.com>
2015-06-12 10:46:03 +0200

    26

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
 			};
 		//in den Link meinen API Key einfügen, dieser sieht man, wenn man bei https://developer.forecast.io/ eingeloggt ist
-
+		//forecast.io Anfrage
 			$.ajax({
 				url: 'https://api.forecast.io/forecast/074dda45e7a36c5645570c91ba46be0f/'+ koordinaten.latitude +','+ koordinaten.longitude,
 				data: {
@@ -23,6 +23,17 @@ $(document).ready(function(){
 				console.log(data);
 				$('.temperatur').text(data.currently.apparentTemperature+ ' °C');
 				$('.description').text(data.currently.summary);
+				//google geocoding anfrage
+				$ajax({
+					url: 'https:maps.googleapis.com/maps/api/geocode/json',
+					data: {
+						latlng: koordinaten.latitude +','+ koordinaten.longitude,
+						key: 'AIzaSyDgYh-UffzCV54XCcReML4WSqyb0_zv8x8',
+						language: 'de'
+					}
+				}).done(function(data)){
+					console.log(data);
+				}
 
 			});
 
@@ -30,3 +41,4 @@ $(document).ready(function(){
 	});
 });
 
+// https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyDgYh-UffzCV54XCcReML4WSqyb0_zv8x8
